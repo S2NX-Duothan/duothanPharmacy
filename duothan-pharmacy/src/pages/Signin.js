5b5f510d5e55dc5e5e31d42b34e8aa5d7cf2ec1e
Hasini Sachintha <108792237+HasiniSachintha@users.noreply.github.com>
2023-02-24 16:19:43 +0530

    signin

--- duothan-pharmacy/src/pages/Signin.js
+++ duothan-pharmacy/src/pages/Signin.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import "../styles/signin.css";
 
 const Signin = () => {
   return (
-    <div>Signin</div>
+    <div className='signin-page'>
+      <div className='signin-image'><img scr="bg.png"></img></div>
+      <div className='signin-info'></div>
+    </div>
   )
 }
 
