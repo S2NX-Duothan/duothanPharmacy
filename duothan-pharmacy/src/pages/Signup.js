900c2247ed62a3aff99db07987c62c02a5799489
Nipun-Milinda <nipunmilinda@gmail.com>
2023-02-24 15:07:44 +0530

    create sign up page

--- duothan-pharmacy/src/pages/Signup.js
+++ duothan-pharmacy/src/pages/Signup.js
@@ -1,8 +1,41 @@
 import React from 'react'
+import "../styles/signup.css"
+
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <div className="main-container" >
+      <div className='main-box'>
+        <div className='title'><center>Sign Up</center></div>
+        <div className='input-items'>
+          <div className='btn-title'><label for="username">Pharmacy name:</label></div>
+          <div className='btn-input'><input type="text" id="username" name="username"></input></div>
+          <div className='btn-title'><label for="username">Email:</label></div>
+          <div className='btn-input'><input type="text" id="username" name="username"></input></div>
+          <div className='btn-title'><label for="username">Address:</label></div>
+          <div className='btn-input'><input type="text" id="username" name="username"></input></div>
+          <div className='btn-title'><label for="username">Phone number:</label></div>
+          <div className='btn-input'><input type="text" id="username" name="username"></input></div>
+          <div className='btn-title'><label for="username">Pharmacy license number:</label></div>
+          <div className='btn-input'><input type="text" id="username" name="username"></input></div>
+          <div className='btn-title'><label for="username">Password:</label></div>
+          <div className='btn-input'><input type="password" id="username" name="username"></input></div>
+          <div className='btn-title'><label for="username">Confirm password:</label></div>
+          <div className='btn-input'><input type="password" id="username" name="username"></input></div>
+        </div>
+        <div className='input-condition'>
+          <input type="checkbox" id="username" name="username"></input>
+          <p>I agree to Terms of Service and Privacy Policy.</p>
+        </div>
+        <div className='submit-btn'>
+          <center><button type="button">Sign Up</button></center>
+        </div>
+        <div className='sign-in'>
+          <p>Already have an account? </p>
+          <a href="#">Sign In</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
